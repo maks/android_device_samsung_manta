e4ef6e7a23b68244807c899eca7322835e94bfdf
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-01-02 08:09:12 +0100

    power:changed default tunnables and removed refresh governor part
    Change-Id: I317296dcee6759dfd7c2311b67bc691968dcfd2d

--- power/power_manta.c
+++ power/power_manta.c
@@ -39,8 +39,8 @@
 #define CPU_MAX_FREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
 #define GOVERNOR_NAME_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 //BOOST_PULSE_DURATION and BOOT_PULSE_DURATION_STR should always be in sync
-#define BOOST_PULSE_DURATION 80000
-#define BOOST_PULSE_DURATION_STR "80000"
+#define BOOST_PULSE_DURATION 200000
+#define BOOST_PULSE_DURATION_STR "200000"
 #define NSEC_PER_SEC 1000000000
 #define USEC_PER_SEC 1000000
 #define NSEC_PER_USEC 100
@@ -56,7 +56,6 @@ struct manta_power_module {
 };
 
 static char scaling_max_freq_screen_on[51];
-static char gov_name[51];
 
 static unsigned int vsync_count;
 static struct timespec last_touch_boost;
@@ -107,13 +106,6 @@ static void sysfs_read(const char *path, char *s,size_t buflen)
     close(fd);
 }
 
-void *refresh_governor(void *param) {
-    for(;;) {
-       sysfs_read(GOVERNOR_NAME_PATH, gov_name, sizeof(gov_name));
-       sleep(300);
-    }
-}
-
 static void power_init(struct power_module *module)
 {
     struct manta_power_module *manta = (struct manta_power_module *) module;
@@ -125,14 +117,14 @@ static void power_init(struct power_module *module)
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
                 "20000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_slack",
-                "70000");
+                "80000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
                 "40000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq",
                 "800000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
                 "99");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "80 1000000:80 1100000:85 1200000:90 1300000:95 1400000:99");
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "70 300000:70 400000:75 500000:80 800000:85 1000000:80 1100000:85 1200000:90 1300000:95 1400000:99");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
                 "80000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration",
@@ -140,11 +132,7 @@ static void power_init(struct power_module *module)
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/sync_freq", "1700000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/up_threshold_any_cpu_load", "95");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/up_threshold_any_cpu_freq", "1500000");
-
-    pthread_t thread_gov;
-
-    pthread_create(&thread_gov, NULL, refresh_governor, NULL);
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/up_threshold_any_cpu_freq", "1400000");
 }
 
 static void power_set_interactive(struct power_module *module, int on)
