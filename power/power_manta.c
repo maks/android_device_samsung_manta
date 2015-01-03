8ebd60a68d909b133c4ae58e56aad73dbee326bd
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-01-03 01:54:17 +0100

    power:cleaned code a bit, now power only handles screen on/off intractions
    Change-Id: If77181fdba54f6eda627d9940cc650815cdb8485

--- power/power_manta.c
+++ power/power_manta.c
@@ -34,16 +34,7 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define BOOST_PATH "/sys/devices/system/cpu/cpufreq/interactive/boost"
 #define CPU_MAX_FREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
-#define GOVERNOR_NAME_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
-//BOOST_PULSE_DURATION and BOOT_PULSE_DURATION_STR should always be in sync
-#define BOOST_PULSE_DURATION 200000
-#define BOOST_PULSE_DURATION_STR "200000"
-#define NSEC_PER_SEC 1000000000
-#define USEC_PER_SEC 1000000
-#define NSEC_PER_USEC 100
 #define LOW_POWER_MAX_FREQ "800000"
 #define NORMAL_MAX_FREQ "1700000"
 
@@ -57,12 +48,7 @@ struct manta_power_module {
 
 static char scaling_max_freq_screen_on[51];
 
-static unsigned int vsync_count;
-static struct timespec last_touch_boost;
-static bool touch_boost;
 static bool low_power_mode = false;
-
-static unsigned int last_gov_check;
 
 static void sysfs_write(const char *path, char *s)
 {
@@ -114,22 +100,8 @@ static void power_init(struct power_module *module)
 
     sysfs_read(CPU_MAX_FREQ_PATH, scaling_max_freq_screen_on, sizeof(scaling_max_freq_screen_on));
 
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
-                "20000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_slack",
-                "80000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "40000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq",
-                "800000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
-                "99");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "70 300000:70 400000:75 500000:80 800000:85 1000000:80 1100000:85 1200000:90 1300000:95 1400000:99");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
-                "80000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration",
-                BOOST_PULSE_DURATION_STR);
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/target_loads", "70 300000:70 400000:75 500000:80 800000:85 1000000:70 1100000:80 1200000:85 1300000:90 1400000:95 15000000:99");
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay","80000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/sync_freq", "1700000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/up_threshold_any_cpu_load", "95");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/up_threshold_any_cpu_freq", "1400000");
@@ -153,9 +125,6 @@ static void power_set_interactive(struct power_module *module, int on)
      */
     sysfs_write(CPU_MAX_FREQ_PATH,
                 (!on || low_power_mode) ? LOW_POWER_MAX_FREQ : scaling_max_freq_screen_on);
-
-
-    sysfs_write(manta->touchscreen_power_path, on ? "Y" : "N");
 
     ALOGV("power_set_interactive: %d done\n", on);
 }
